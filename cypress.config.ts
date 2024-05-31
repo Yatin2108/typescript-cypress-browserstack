42c6c17390c348dc03db2b166ae81d19c14cb4c4
Yatin2108 <yatin@browserstack.com>
2024-05-31 16:35:05 +0530

    observability integration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
+const browserstackTestObservabilityPlugin = require('browserstack-cypress-cli/bin/testObservability/plugin');
 
 module.exports = defineConfig({
   experimentalWebKitSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      browserstackTestObservabilityPlugin(on, config);
+      return config;
     },
   },
 });
